da405666a4f6d4c7dd458e8088b258e8f2a5f467
LizethMReyes <lizethmreyesg@gmail.com>
2022-03-29 16:32:22 -0600

    Tiene ruta de repositorio

--- R/histo.R
+++ R/histo.R
@@ -1,4 +1,4 @@
-setwd("/Users/lizethreyes/Desktop/Proyecto/seleccion_analista_2022_lmrg/pollos_petrel")
-train_bp <- read.csv("train.csv")
+train_bp <- read.csv("seleccion_analista_2022_lmrg/pollos_petrel/train.csv")
 #Crea un histograma de longitud de tarso
 hist(train_bp$Longitud_tarso, main = "Longitud de tarso", xlab= "Longitud tarso", ylab="Frecuencia")
+
