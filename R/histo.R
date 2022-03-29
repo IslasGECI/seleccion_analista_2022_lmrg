d0ff044f08840b5de78a3c0e793a1a4eed383c74
LizethMReyes <lizethmreyesg@gmail.com>
2022-03-29 17:36:50 -0600

    Elimina lectura de csv duplicada

--- R/histo.R
+++ R/histo.R
@@ -1,6 +1,5 @@
 
 train_bp <- read.csv("seleccion_analista_2022_lmrg/pollos_petrel/train.csv")
-train_bp <- read.csv("train.csv")
 #Crea un histograma de longitud de tarso
 hist(train_bp$Longitud_tarso, main = "Longitud de tarso", xlab= "Longitud tarso", ylab="Frecuencia")
 
