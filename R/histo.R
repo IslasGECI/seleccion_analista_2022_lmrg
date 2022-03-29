075025a1535ffafe2e256c477e7abbc6bf6c0fd0
LizethMReyes <lizethmreyesg@gmail.com>
2022-03-29 13:00:43 -0600

    Quita el destino de la carpeta local

--- R/histo.R
+++ R/histo.R
@@ -1,3 +1,3 @@
 train_bp <- read.csv("train.csv")
-#Crea un histograma de longitud de tarso
+#Crea un histograma de longitud de tarso.
 hist(train_bp$Longitud_tarso, main = "Longitud de tarso", xlab= "Longitud tarso", ylab="Frecuencia")
