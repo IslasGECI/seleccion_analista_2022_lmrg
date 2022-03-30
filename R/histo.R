2f994d0a0503a812449e12a9d866e18b786f7f81
LizethMReyes <lizethmreyesg@gmail.com>
2022-03-29 18:14:20 -0600

    Nueva ruta

--- R/histo.R
+++ R/histo.R
@@ -1,5 +1,5 @@
 
-train_bp <- read.csv("seleccion_analista_2022_lmrg/pollos_petrel/train.csv")
+train_bp <- read.csv("pollos_petrel/train.csv")
 #Crea un histograma de longitud de tarso
 hist(train_bp$Longitud_tarso, main = "Longitud de tarso", xlab= "Longitud tarso", ylab="Frecuencia")
 
