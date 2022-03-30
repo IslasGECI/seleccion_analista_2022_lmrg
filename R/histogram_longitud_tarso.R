7ae5fef95f59fab7cc140429103a45e6e31b0eda
LizethMReyes <lizethmreyesg@gmail.com>
2022-03-29 19:49:03 -0600

    Modifica el nombre de la variable

--- R/histogram_longitud_tarso.R
+++ R/histogram_longitud_tarso.R
@@ -1,7 +1,7 @@
-read_training_dataset <- read.csv("pollos_petrel/train.csv")
+training_dataset <- read.csv("pollos_petrel/train.csv")
 # Crea un histograma de longitud de tarso
 hist(
-  read_training_dataset$Longitud_tarso,
+  training_dataset$Longitud_tarso,
   main = "Longitud de tarso",
   xlab = "Longitud tarso",
   ylab = "Frecuencia"
