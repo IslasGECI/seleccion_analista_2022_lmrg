53a875e16e18bff62e42a8293cef04c6c64a5b0c
LizethMReyes <lizethmreyesg@gmail.com>
2022-04-09 17:19:11 -0500

    Convierte histogram_longitud_tarso.R en una función y modifica el nombre test_histogram

--- R/histogram_longitud_tarso.R
+++ R/histogram_longitud_tarso.R
@@ -1,8 +1,10 @@
 training_dataset <- read.csv("pollos_petrel/train.csv")
 # Crea un histograma de longitud de tarso
-hist(
-  training_dataset$Longitud_tarso,
-  main = "Longitud de tarso",
-  xlab = "Longitud tarso",
-  ylab = "Frecuencia"
-)
+print_hist_longitud_tarso <- function() {
+  hist(
+    training_dataset$Longitud_tarso,
+    main = "Longitud de tarso",
+    xlab = "Longitud tarso",
+    ylab = "Frecuencia"
+  )
+}
--- tests/testthat/test_histogram.R
+++ tests/testthat/test_histogram.R
@@ -1,8 +1,10 @@
-test_that("Print hist", {
-  disp_hist_base <- function() hist(mtcars$disp)
-  disp_hist_ggplot <- ggplot(mtcars, aes(disp)) +
-    geom_histogram()
+ library("ggplot2")
 
-  vdiffr::expect_doppelganger("Base graphics histogram", disp_hist_base)
-  vdiffr::expect_doppelganger("ggplot2 histogram", disp_hist_ggplot)
-})
+  test_that("Print longitud tarso", {
+    data_fake<-data.frame("longitud_fake"=c(12,12,12,12,17.2,17.2,17.2,17.2,17.2,17.2,20,20,20,20,20,20))
+    disp_hist_base <- function() hist(data_fake$longitud_fake)
+   vdiffr:: expect_doppelganger("disp-histogram-base", disp_hist_base)
+
+     vdiffr::expect_doppelganger("Histogram longitud tarso",print_hist_longitud_tarso)
+
+  })
