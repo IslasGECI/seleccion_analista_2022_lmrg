dbe4e71a169988f37aff0a28731a7d17ed1484b5
LizethMReyes <lizethmreyesg@gmail.com>
2022-04-09 22:30:17 -0500

    Modifica la funcion a print_hist_longitud_tarso

--- tests/testthat/test_histogram.R
+++ tests/testthat/test_histogram.R
@@ -1,10 +1,5 @@
  library("ggplot2")
 
-  test_that("Print longitud tarso", {
-    data_fake<-data.frame("longitud_fake"=c(12,12,12,12,17.2,17.2,17.2,17.2,17.2,17.2,20,20,20,20,20,20))
-    disp_hist_base <- function() hist(data_fake$longitud_fake)
-   vdiffr:: expect_doppelganger("disp-histogram-base", disp_hist_base)
-
-     vdiffr::expect_doppelganger("Histogram longitud tarso",print_hist_longitud_tarso)
-
-  })
+ test_that("Print longitud tarso", {
+   vdiffr::expect_doppelganger("Histogram longitud tarso", print_hist_longitud_tarso)
+ })
