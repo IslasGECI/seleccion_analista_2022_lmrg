6cad7741137d0ea0fe66b2aac04fc72cc64d0a70
LizethMReyes <lizethmreyesg@gmail.com>
2022-04-10 10:16:13 -0500

    Cambia el formato con Styler

--- tests/testthat/test_histogram.R
+++ tests/testthat/test_histogram.R
@@ -1,5 +1,4 @@
- library("ggplot2")
-
- test_that("Print longitud tarso", {
-   vdiffr::expect_doppelganger("Histogram longitud tarso", print_hist_longitud_tarso)
- })
+library("ggplot2")
+test_that("Print longitud tarso", {
+  vdiffr::expect_doppelganger("Histogram longitud tarso", print_hist_longitud_tarso)
+})
