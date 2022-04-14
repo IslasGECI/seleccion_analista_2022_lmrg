91c18f36de9b2b98beca2a3e0a1d62f7504bb091
LizethMReyes <lizethmreyesg@gmail.com>
2022-04-14 18:20:37 -0500

    Agrega lm

--- tests/testthat/test_model.R
+++ tests/testthat/test_model.R
@@ -1,23 +1,24 @@
 library(testthat)
 library(tidyverse)
-library(skimr)
-#Test del modelo de regresion líneal múltiple
+set.seed(2)
+dataset <- tibble(id = (1:100), Masa = (101:200), L_tars = rnorm(100), L_ala = rnorm(100), L_pico = rnorm(100), targ = (1:100))
+# Test del modelo de regresion líneal múltiple
 test_that("Create model Linel Regression Multiple", {
-  set.seed(2)
-  dataset<-tibble(id = (1:100), Masa=(101:200), L_tars=rnorm(100), L_ala=rnorm(100), L_pico=rnorm(100), targ = (1:100))
-  lm_dt <- lm(dataset$targ~ dataset$Masa + dataset$L_tars + dataset$L_ala + dataset$L_pico)
+  lm_dt <- lm(dataset$targ ~ dataset$Masa + dataset$L_tars + dataset$L_ala + dataset$L_pico)
   expect_that(lm_dt, model_target)
 })
-#Test target predict
+# Test target predict
 test_that("Get target", {
   set.seed(2)
-  dataset_test<-tibble(id = (1:100), Masa=(101:200), L_tars=rnorm(100), L_ala=rnorm(100), L_pico=rnorm(100))
-  target_fake<- predict(object = lm_dt, newdata = dataset_test, interval = "confidence", level = 0.95)
-  expect_that( target_fake, predict_target)
+  dataset <- tibble(id = (1:100), Masa = (101:200), L_tars = rnorm(100), L_ala = rnorm(100), L_pico = rnorm(100), targ = (1:100))
+  dataset_test <- tibble(id = (1:100), Masa = (101:200), L_tars = rnorm(100), L_ala = rnorm(100), L_pico = rnorm(100))
+  lm_dt <- lm(dataset$targ ~ dataset$Masa + dataset$L_tars + dataset$L_ala + dataset$L_pico)
+  target_fake <- predict(object = lm_dt, newdata = dataset_test, interval = "confidence", level = 0.95)
+  expect_that(target_fake, predict_target)
 })
-#Test export predict
+# Test export predict
 test_that("Get target", {
-  export_target_fake <-write.csv(target_fake)
-  expect_that( export_target_fake, export_csv)
+  export_target_fake <- write.csv(target_fake)
+  expect_that(export_target_fake, export_csv)
 })
 
