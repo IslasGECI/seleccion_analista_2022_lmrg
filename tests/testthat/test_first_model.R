b14ac9e97139f7c48077447d5468c7b75e52cce0
LizethMReyes <lizethmreyesg@gmail.com>
2022-05-31 11:59:23 -0500

    Add failing test
    Agregamo una prueba para contar el número de filas, count no funciona para esto.

--- tests/testthat/test_first_model.R
+++ tests/testthat/test_first_model.R
@@ -14,9 +14,14 @@ describe("write dataframe with target column equal one", {
     obtained <- target_equal_one(3)
     expect_equal(obtained, expected)
   })
-  it("three rows", {
+  it("four rows", {
     expected <- tibble(id = 1:4, target = rep(1,4))
     obtained <- target_equal_one(4)
     expect_equal(obtained, expected)
+  })
+  it("327 rows", {
+    expected_n_rows <- 327
+    obtained_n_rows <- count(target_equal_one(327),"id")
+    expect_equal(obtained_n_rows, expected_n_rows)
   })
 })
