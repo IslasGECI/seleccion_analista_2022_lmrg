74916b56bbacfe9e9f6e87505fa00942a3ec1223
LizethMReyes <lizethmreyesg@gmail.com>
2022-05-31 11:56:13 -0500

    Add failing tests Agregamos prueba para calcular la cantidad de renglones, 'count' no soluicona el problema.

--- tests/testthat/test_first_model.R
+++ tests/testthat/test_first_model.R
@@ -3,10 +3,20 @@ library(tidyverse)
 
 dataset <- tibble(id = 1:2, target = 3:4)
 # Lee train.csv
-describe("Read training dataset", {
-  it("reads all 1304 records", {
+describe("write dataframe with target column equal one", {
+  it("two rows", {
     expected <- tibble(id = 1:2, target = rep(1,2))
-    obtained <- target_equal_one()
+    obtained <- target_equal_one(2)
+    expect_equal(obtained, expected)
+  })
+  it("three rows", {
+    expected <- tibble(id = 1:3, target = rep(1,3))
+    obtained <- target_equal_one(3)
+    expect_equal(obtained, expected)
+  })
+  it("three rows", {
+    expected <- tibble(id = 1:4, target = rep(1,4))
+    obtained <- target_equal_one(4)
     expect_equal(obtained, expected)
   })
 })
