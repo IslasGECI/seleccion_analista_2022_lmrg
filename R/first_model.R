414a23dd4e4c3635d47bcddabd9487fa2b61f1e6
LizethMReyes <lizethmreyesg@gmail.com>
2022-05-31 23:13:59 -0500

    Apply code formatter
    Aplica styler para seguir con la guía de estilo.

--- R/first_model.R
+++ R/first_model.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-target_equal_one <- function(n_rows){
-  table <- tibble(id = 1:n_rows, target = rep(1,n_rows))
+target_equal_one <- function(n_rows) {
+  table <- tibble(id = 1:n_rows, target = rep(1, n_rows))
   return(table)
 }
--- tests/testthat/test_first_model.R
+++ tests/testthat/test_first_model.R
@@ -5,17 +5,17 @@ dataset <- tibble(id = 1:2, target = 3:4)
 # Lee train.csv
 describe("write dataframe with target column equal one", {
   it("two rows", {
-    expected <- tibble(id = 1:2, target = rep(1,2))
+    expected <- tibble(id = 1:2, target = rep(1, 2))
     obtained <- target_equal_one(2)
     expect_equal(obtained, expected)
   })
   it("three rows", {
-    expected <- tibble(id = 1:3, target = rep(1,3))
+    expected <- tibble(id = 1:3, target = rep(1, 3))
     obtained <- target_equal_one(3)
     expect_equal(obtained, expected)
   })
   it("four rows", {
-    expected <- tibble(id = 1:4, target = rep(1,4))
+    expected <- tibble(id = 1:4, target = rep(1, 4))
     obtained <- target_equal_one(4)
     expect_equal(obtained, expected)
   })
