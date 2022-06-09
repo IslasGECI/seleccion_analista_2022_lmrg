44022a4b6933ab7f0d290884d6566fea948e72d0
LizethMReyes <lizethmreyesg@gmail.com>
2022-06-09 11:17:13 -0500

    Delete table
    Elimina la creación de tabla que contiene el argumento nrows.

--- R/first_model.R
+++ R/first_model.R
@@ -1,5 +1,11 @@
 library(tidyverse)
 target_equal_one <- function(n_rows) {
-  table <- tibble(id = 1:n_rows, target = rep(1, n_rows))
-  return(table)
+  target_mean <- get_target()
+  testing_dataset <- read_testing_dataset()
+  submission <- drop_all_but_id(testing_dataset) %>%
+    mutate("target" = target_mean)
+  return(submission)
+}
+get_target <- function(){
+  return(1)
 }
--- tests/testthat/test_first_model.R
+++ tests/testthat/test_first_model.R
@@ -4,24 +4,9 @@ library(tidyverse)
 dataset <- tibble(id = 1:2, target = 3:4)
 # Lee train.csv
 describe("write dataframe with target column equal one", {
-  it("two rows", {
-    expected <- tibble(id = 1:2, target = rep(1, 2))
-    obtained <- target_equal_one(2)
-    expect_equal(obtained, expected)
-  })
-  it("three rows", {
-    expected <- tibble(id = 1:3, target = rep(1, 3))
-    obtained <- target_equal_one(3)
-    expect_equal(obtained, expected)
-  })
-  it("four rows", {
-    expected <- tibble(id = 1:4, target = rep(1, 4))
-    obtained <- target_equal_one(4)
-    expect_equal(obtained, expected)
-  })
-  it("327 rows", {
-    expected_n_rows <- 327
-    obtained_n_rows <- nrow(target_equal_one(327))
+  it("326 rows", {
+    expected_n_rows <- 326
+    obtained_n_rows <- nrow(target_equal_one(326))
     expect_equal(obtained_n_rows, expected_n_rows)
   })
 })
