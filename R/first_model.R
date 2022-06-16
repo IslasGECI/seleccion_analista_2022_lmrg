63dee02d78f966e84bcae8c71926546a85bf4ce2
LizethMReyes <lizethmreyesg@gmail.com>
2022-06-16 11:23:58 -0500

    Write submissions by target
    Escribe sumbmission para target.

--- R/first_model.R
+++ R/first_model.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-target_equal_one <- function(n_rows) {
+target_equal_one <- function() {
   target_mean <- get_target()
   testing_dataset <- read_testing_dataset()
   submission <- drop_all_but_id(testing_dataset) %>%
@@ -8,4 +8,11 @@ target_equal_one <- function(n_rows) {
 }
 get_target <- function() {
   return(1)
+}
+
+#' @export
+write_lizeth_submission <- function() {
+  submission_path <- "pollos_petrel/example_lizeth_submission.csv"
+  submission <- target_equal_one()
+  write_csv(submission, submission_path)
 }
