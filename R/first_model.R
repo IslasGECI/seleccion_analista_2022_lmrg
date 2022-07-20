f6eecfc6b96ac35cc0cb4c9f250bdfee39920ffc
LizethMReyes <lizethmreyesg@gmail.com>
2022-07-20 15:13:41 -0500

    Add lineal model and mean target
    Agrega dos modelos, el primero es lineal con la variable Masa como variable dependiente y el segundo es el promedio de target.

--- R/first_model.R
+++ R/first_model.R
@@ -7,12 +7,27 @@ target_equal_one <- function() {
   return(submission)
 }
 get_target <- function() {
-  return(1)
+  target_mean <- mean(training_dataset$target)
+  training_dataset <- read_training_dataset()
+  return(target_mean)
 }
-
+get_age_by_mass <- function(Masa) {
+  age <- -4.8192 + Masa * 0.6832
+  return(age)
+}
+target_lineal_model <- function() {
+  testing_dataset <- read_testing_dataset()
+  age <- get_age_by_mass(testing_dataset$Masa)
+  submission <- drop_all_but_id(testing_dataset) %>%
+    mutate("target" = age)
+  return(submission)
+}
 #' @export
 write_lizeth_submission <- function() {
   submission_path <- "pollos_petrel/example_lizeth_submission.csv"
   submission <- target_equal_one()
+  write_csv(submission, submission_path)
+  submission_path <- "pollos_petrel/example_lizeth_submission_lineal.csv"
+  submission <- target_lineal_model()
   write_csv(submission, submission_path)
 }
