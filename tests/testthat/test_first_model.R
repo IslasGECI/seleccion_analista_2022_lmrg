235a85b58e5d817d8fdcb1ab6fc5417fbded5b7b
LizethMReyes <lizethmreyesg@gmail.com>
2022-07-29 12:46:33 -0500

    Add tests for target lineal model
    Agregamos pruebas para número de columnas, núm. de filas y nombre de columnas.

--- tests/testthat/test_first_model.R
+++ tests/testthat/test_first_model.R
@@ -17,3 +17,20 @@ describe("write dataframe with target calculate lineal model", {
     expect_equal(obtained_b, expected_b)
   })
 })
+describe("write dataframe with target column equal age, target_lineal_model", {
+  it("326 rows", {
+    expected_n_rows <- 326
+    obtained_n_rows <- nrow(read_testing_dataset())
+    expect_equal(obtained_n_rows, expected_n_rows)
+  })
+  it("2 columns", {
+    expected_n_columns <- 2
+    obtained_n_columns <- ncol(target_lineal_model())
+    expect_equal(obtained_n_columns, expected_n_columns)
+  })
+  it("columns: id, target", {
+    expected_name_columns <- c("id", "target")
+    obtained_name_columns <- names(target_lineal_model())
+    expect_equal(obtained_name_columns, expected_name_columns)
+  })
+})
